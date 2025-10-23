bcb6836c702b832d025ccdec4814738d03bc01c1
Jorge Rodrigues <jorgegabriel99h@gmail.com>
2025-10-23 17:13:51 -0300

    feat(Eproject): add new endpoint /event - deposit

--- app.rb
+++ app.rb
@@ -27,3 +27,21 @@ get '/balance' do
     '0'
   end
 end
+
+# ----------------------------
+# Event handler (deposit, withdraw, transfer)
+# ----------------------------
+post '/event' do
+  # Parse JSON body with symbol keys
+  data = JSON.parse(request.body.read, symbolize_names: true)
+
+  # Deposit event
+  destination = data[:destination]
+  amount = data[:amount]
+
+  $accounts[destination] ||= 0
+  $accounts[destination] += amount
+
+  status 201
+  { destination: { id: destination, balance: $accounts[destination] } }.to_json
+end
