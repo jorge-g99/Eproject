6dfa67267a46903c4857797c8e54c53ba36b82c5
Jorge Rodrigues <jorgegabriel99h@gmail.com>
2025-10-23 16:59:06 -0300

    feat(Eproject): add new endpoint /reset

--- app.rb
+++ app.rb
@@ -1,1 +1,14 @@
 require 'sinatra'
+
+$accounts = {}
+
+set :bind, '0.0.0.0'
+
+# ----------------------------
+# Reset all accounts
+# ----------------------------
+post '/reset' do
+  $accounts.clear
+  status 200
+  'OK'
+end
