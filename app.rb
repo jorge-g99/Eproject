511bf3cb44c097d8bcdbe26bda06a8c23d9391f8
Jorge Rodrigues <jorgegabriel99h@gmail.com>
2025-10-23 17:06:10 -0300

    feat(Eproject): add new endpoint /balance

--- app.rb
+++ app.rb
@@ -12,3 +12,18 @@ post '/reset' do
   status 200
   'OK'
 end
+
+# ----------------------------
+# Get account balance
+# ----------------------------
+get '/balance' do
+  account_id = params['account_id']
+  
+  $accounts[account_id]&.then do |balance|
+    status 200
+    balance.to_s
+  end || begin
+    status 404
+    '0'
+  end
+end
